d0ede9a446177c1f392a97c56b055969718ce13a
jeff-mcc <reffery@gmail.com>
2021-04-20 19:04:42 -0600

    completed P portion of the assignment

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react'
 
 class Palindrome extends Component{
+    constructor(){
+        super()
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+        this.solutionP = this.solutionP.bind(this)
+        this.receiveInput = this.receiveInput.bind(this)
+    }
+
+    receiveInput(val){
+        this.setState({userInput: val})
+    }
+
+    solutionP(){
+        let prop = this.state.userInput;
+        let output = '';
+        for (let i = 0; i<prop.length; i++){
+            if (prop[i] !== prop[prop.length-1-i]){
+                return this.setState({palindrome: 'not a palindrome'})
+            }
+        }
+        output = `${prop} is a palindrome!`
+        return this.setState({palindrome: output})
+        // console.log('test')
+    }
+
     render(){
         return(
-            <p>Palindrome</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(e)=>this.receiveInput(e.target.value)}/>
+                <button className="confirmationButton" onClick={this.solutionP}>Check</button>
+                <span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
